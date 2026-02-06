b1294623c7ec83b9f0f8dd9171b0caad75178c4c
Alec Brown <alec.r.brown@oracle.com>
2026-02-06 02:47:27 +0000

    [revision] for x86/txt: Intel trusted eXecution Technology (TXT)
    Add function to parse the addresses of each table in the TXT heap memory.
    
    Signed-off-by: Alec Brown <alec.r.brown@oracle.com>

--- arch/x86/include/asm/txt.h
+++ arch/x86/include/asm/txt.h
@@ -84,11 +84,13 @@
 /*
  * TXT Heap Table Enumeration
  */
-#define TXT_BIOS_DATA_TABLE		1
-#define TXT_OS_MLE_DATA_TABLE		2
-#define TXT_OS_SINIT_DATA_TABLE		3
-#define TXT_SINIT_MLE_DATA_TABLE	4
-#define TXT_SINIT_TABLE_MAX		TXT_SINIT_MLE_DATA_TABLE
+enum {
+	TXT_BIOS_DATA_TABLE,
+	TXT_OS_MLE_DATA_TABLE,
+	TXT_OS_SINIT_DATA_TABLE,
+	TXT_SINIT_MLE_DATA_TABLE,
+	TXT_SINIT_TABLE_MAX,
+}
 
 #ifndef __ASSEMBLER__
 
@@ -250,51 +252,22 @@ struct tpm_event_log_header {
  *
  *  NOTE: the table size fields include the 8 byte size field itself.
  */
-static inline u64 txt_bios_data_size(void *heap)
+
+struct txt_heap_info {
+	u64 size;
+	u32 offset;
+} __packed;
+
+static inline void txt_parse_heap(void *heap, struct txt_heap_info *txt_heap_ptrs[TXT_SINIT_TABLE_MAX])
 {
-	return *((u64 *)heap);
+	void *tmp = heap;
+	for (u8 i; i < TXT_SINIT_TABLE_MAX; i++) {
+		txt_heap_ptrs[i]->size = *((u64 *) tmp);
+		txt_heap_ptrs[i]->offset = tmp - heap + sizeof(txt_heap_ptrs[i]->size);
+		tmp += txt_heap_ptrs[i]->size;
+	}
 }
 
-static inline void *txt_bios_data_start(void *heap)
-{
-	return heap + sizeof(u64);
-}
-
-static inline u64 txt_os_mle_data_size(void *heap)
-{
-	return *((u64 *)(heap + txt_bios_data_size(heap)));
-}
-
-static inline void *txt_os_mle_data_start(void *heap)
-{
-	return heap + txt_bios_data_size(heap) + sizeof(u64);
-}
-
-static inline u64 txt_os_sinit_data_size(void *heap)
-{
-	return *((u64 *)(heap + txt_bios_data_size(heap) +
-			txt_os_mle_data_size(heap)));
-}
-
-static inline void *txt_os_sinit_data_start(void *heap)
-{
-	return heap + txt_bios_data_size(heap) +
-		txt_os_mle_data_size(heap) + sizeof(u64);
-}
-
-static inline u64 txt_sinit_mle_data_size(void *heap)
-{
-	return *((u64 *)(heap + txt_bios_data_size(heap) +
-			txt_os_mle_data_size(heap) +
-			txt_os_sinit_data_size(heap)));
-}
-
-static inline void *txt_sinit_mle_data_start(void *heap)
-{
-	return heap + txt_bios_data_size(heap) +
-		txt_os_mle_data_size(heap) +
-		txt_os_sinit_data_size(heap) + sizeof(u64);
-}
 
 /*
  * Find the TPM v2 event log element in the TXT heap. This element contains
